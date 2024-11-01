28a15ece10ae5b5a0e8ef4d665371fb59cd1734d
Karina De Almeida Lima <karinadealmeidalima@MacBook-Pro-de-Karina.local>
2024-11-01 06:05:17 -0300

    criando microfrontends

--- shared-routing/Shell/webpack.config.js
+++ shared-routing/Shell/webpack.config.js
@@ -1,9 +1,9 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const ModuleFederationPlugin = require("webpack").container
-//   .ModuleFederationPlugin;
+const ModuleFederationPlugin = require("webpack").container
+  .ModuleFederationPlugin;
 
 const path = require("path");
-const deps = require("./package.json").dependencies;
+const deps = require("./package.json").dependencies; // pega todas as deps
 module.exports = {
   entry: "./src/index",
   mode: "development",
@@ -50,35 +50,41 @@ module.exports = {
     ],
   },
   plugins: [
-    // new ModuleFederationPlugin({
-    //   name: "shell",
-    //   filename: "remoteEntry.js",
-    //   remotes: {
-    //     productdetails: "productdetails@http://localhost:3002/remoteEntry.js",
-    //     productlist: "productlist@http://localhost:3001/remoteEntry.js",
-    //     shell: "shell@http://localhost:3000/remoteEntry.js",
-    //   },
-    //   exposes: {
-    //     "./Shell": "./src/Shell",
-    //     "./Service": "./src/Service",
-    //   },
-    //   shared: [
-    //     {
-    //       ...deps,
-    //       react: {
-    //         singleton: true,
-    //         requiredVersion: deps.react,
-    //       },
-    //       "react-dom": {
-    //         singleton: true,
-    //         requiredVersion: deps["react-dom"],
-    //       },
-    //     },
-    //     "./src/Service",
-    //   ],
-    // }),
+    new ModuleFederationPlugin({
+      name: "shell",
+      filename: "remoteEntry.js", // arquivo compartilhado entre apps
+      remotes: {
+        productdetails: "productdetails@http://localhost:3002/remoteEntry.js",
+        productlist: "productlist@http://localhost:3001/remoteEntry.js",
+        shell: "shell@http://localhost:3000/remoteEntry.js",
+      },
+      exposes: {
+        // o que será compartilhado
+        "./Shell": "./src/Shell",
+        "./Service": "./src/Service",
+      },
+      shared: [
+        // vantagem de compartilhar dependências, instância só o que precisa
+        {
+          ...deps,
+          react: {
+            singleton: true, // estratégia de compartilhamento: apenas uma instância
+            requiredVersion: deps.react, // se tiver 2 microfrontens com a mesma versão, da onde vai pegar a versão
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: deps["react-dom"],
+          },
+        },
+        "./src/Service",
+      ],
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
   ],
 };
+
+// erro: Shared module is not available for eager consumption: webpack/sharing/consume/default/react/react
+// erro que ocorre pq o webpack precisa saber antes da app subir como resolver essas deps,
+// bidirecional: quando consume e quando exporta deps
